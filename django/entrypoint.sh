5477e0158cf151870982d54cfc6b3e3ce6a6428f
Júlio Orlando <falecomjulioorlando@outlook.com>
2026-09-21 14:30:59 -0300

    feat(django): seed SaaS modules and periodic jobs on deploy

--- django/entrypoint.sh
+++ django/entrypoint.sh
@@ -2,6 +2,8 @@
 set -eu
 
 python manage.py migrate --noinput
+python manage.py seed_modules
+python manage.py seed_periodic_tasks
 python manage.py collectstatic --noinput
 
 exec gunicorn applanner.wsgi:application \
