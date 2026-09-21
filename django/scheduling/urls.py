0e4807ea04e520203bcb1b06a654ff8e457b1f2a
Júlio Orlando <falecomjulioorlando@outlook.com>
2026-09-21 14:01:52 -0300

    feat(django): add scheduling domain

---
+++ django/scheduling/urls.py
@@ -0,0 +1,6 @@
+from rest_framework.routers import DefaultRouter
+from .views import AppointmentViewSet
+
+router=DefaultRouter()
+router.register("appointments",AppointmentViewSet,basename="appointment")
+urlpatterns=router.urls
