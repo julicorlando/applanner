dbfc290c2841fe99181cc6360c0f852f8b9f3629
Júlio Orlando <falecomjulioorlando@outlook.com>
2026-09-21 14:24:38 -0300

    feat(django): expose internal availability endpoint

--- django/scheduling/urls.py
+++ django/scheduling/urls.py
@@ -1,6 +1,13 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
+
+from .api import AvailabilityAPIView
 from .views import AppointmentViewSet
 
 router=DefaultRouter()
 router.register("appointments",AppointmentViewSet,basename="appointment")
-urlpatterns=router.urls
+
+urlpatterns=[
+    path("availability/",AvailabilityAPIView.as_view(),name="availability"),
+]
+urlpatterns += router.urls
