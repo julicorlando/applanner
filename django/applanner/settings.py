216e63cb95fdffeac94e7dffcb244f785fe063d7
Júlio Orlando <falecomjulioorlando@outlook.com>
2026-09-22 13:06:33 -0300

    fix(django): register project static directory

--- django/applanner/settings.py
+++ django/applanner/settings.py
@@ -68,6 +68,7 @@ TIME_ZONE=env("TIME_ZONE",default="America/Recife")
 USE_I18N=True
 USE_TZ=True
 STATIC_URL="/static/"
+STATICFILES_DIRS=[BASE_DIR/"static"]
 STATIC_ROOT=BASE_DIR/"staticfiles"
 MEDIA_URL="/media/"
 MEDIA_ROOT=BASE_DIR/"media"
