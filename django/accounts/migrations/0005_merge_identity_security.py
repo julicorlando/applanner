1e28b08916a498e5e7b09d5a9934a46a9d3848f7
Júlio Orlando <falecomjulioorlando@outlook.com>
2026-09-23 09:43:35 -0300

    fix(migrations): merge legacy parity migration branches

---
+++ django/accounts/migrations/0005_merge_identity_security.py
@@ -0,0 +1,9 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies=[
+        ("accounts","0004_legacy_identity_state"),
+        ("accounts","0004_security_parity"),
+    ]
+    operations=[]
