c52f34b9f7106cfb900a7484ce70f767292eb69c
Júlio Orlando <falecomjulioorlando@outlook.com>
2026-09-23 09:36:42 -0300

    feat(public): add professional and appointment management pages

--- django/applanner/urls.py
+++ django/applanner/urls.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from django.urls import include, path
 
 from billing.webhooks import mercadopago_platform_webhook, mercadopago_tenant_webhook
-from core.views import healthz, home, tenant_public
+from core.views import healthz, home, professional_public, tenant_public
 from communications.views import marketing_click, marketing_open, whatsapp_webhook
 from contenthub.views import blog_post, landing, public_directory
+from scheduling.public_views import appointment_page
 from scheduling.public_api import (
     CustomerAppointmentAPIView,
     PublicAvailabilityAPIView,
@@ -30,7 +31,9 @@ urlpatterns = [
     path("commercial/", include("commercial.urls")),
     path("master/", include("core.master_urls")),
     path("directory/", public_directory, name="public-directory"),
+    path("p/<slug:slug>/profissional/<slug:professional_slug>/", professional_public, name="professional-public"),
     path("p/<slug:slug>/", tenant_public, name="tenant-public"),
+    path("agendamento/<str:token>/", appointment_page, name="public-appointment-page"),
     path("blog/<slug:slug>/", blog_post, name="blog-post"),
     path("landing/<slug:slug>/", landing, name="landing"),
     path("tracking/email/<uuid:token>/open.gif", marketing_open, name="marketing-open"),
